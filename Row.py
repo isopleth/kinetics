b90f7dadfd72c7bab0ab16cc6e17dd97d107feff
Jason Leake <jason@leake.me.uk>
2020-02-23 12:05:13 +0000

    update

---
+++ Row.py
@@ -0,0 +1,71 @@
+import array
+from decimal import Decimal
+import sys
+from math import sqrt
+
+class Row:
+    """ Represents a row from the input file
+    """
+    
+    def __init__(self, line):
+        """ Initializer
+        """
+        # True if this row is to be skipped because it contains a syntax
+        # error etc
+        self.skip = False
+        self._epoch = None
+        self._totalAcc = None
+        self.timestamp = None
+
+        line = line.strip()
+        fields = line.split(",")
+        if len(fields) != 4:
+            print(f"Ignore {line}, {len(fields)} fields", file=sys.stderr)
+            self.skip = True
+            return
+
+        if fields[0] == "datetime":
+            print(f"Skip header line {line}", file=sys.stderr)
+            self.skip = True
+            return
+            
+        self.timestamp = fields[0].strip()
+        try:
+            self.val = array.array('d', [Decimal(fields[1]),
+                                         Decimal(fields[2]),
+                                         Decimal(fields[3])])
+        except ValueError:
+            print(f"Conversion error, ignore {line}", file=sys.stderr)
+            self.skip = True
+
+    def __str__(self):
+        """ String representation of row"""
+        return "{},{:.03f},{:.06f},{:.06f},{:.06f},{:.06f}".format(
+            self.timestamp, self.getEpoch(), self.val[0],
+            self.val[1], self.val[2], self.getTotAcc())
+            
+    def getEpoch(self):
+        """ Get the timestamp as an epoch, seconds since 1/1/1970
+        """
+        if self.skip:
+            return None
+        # Lazy evaluation
+        if self._epoch is None:
+            timestring, dot, milliseconds = self.timestamp.partition('.')
+
+            dateObject = time.strptime(timestring, "%Y-%m-%d %H:%M:%S")
+            self._epoch = time.mktime(dateObject) + int(milliseconds) / 1000
+        return self._epoch
+
+    def getTotAcc(self):
+        """ Get total acceleration for the three x,y,z values.
+        """
+        if self.skip:
+            return None
+        # Lazy evaluation
+        if self._totalAcc is None:
+            self._totalAcc = 0
+            for val in self.val:
+                self._totalAcc = self._totalAcc + val*val
+            self._totalAcc = sqrt(self._totalAcc)
+        return self._totalAcc
---
+++ ax3_descriptive_stats.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# coding=UTF-8
+#
+# BSD 2-Clause License
+#
+# Copyright (c) 2019, Jason Leake
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Descriptive statistics for accelerometer file
+#
+
+import numpy as np
+import argparse
+import sys
+from tkinter import filedialog
+import tkinter as tk
+
+from Row import Row
+
+class Processor:
+        
+    def process(self, filename):
+        # Count number of lines in file to get array dimension
+        count = 0
+        with open(filename, "r") as self.fh:
+            line = self.fh.readline().strip()
+            while line:
+                row = Row(line)
+                if row.skip:
+                    pass
+                else:
+                    count += 1
+                line = self.fh.readline().strip()
+            
+        self.x = np.zeros((count,))
+        self.y = np.zeros((count,))
+        self.z = np.zeros((count,))
+        self.tot = np.zeros((count,))
+
+        with open(filename, "r") as self.fh:
+            line = self.fh.readline().strip()
+            index = 0
+            while line:
+                row = Row(line)
+                if row.skip:
+                    pass
+                else:
+                    self.x[index] = row.val[0]
+                    self.y[index] = row.val[1]
+                    self.z[index] = row.val[2]
+                    self.tot[index] = row.getTotAcc()
+                    index += 1
+                line = self.fh.readline().strip()
+
+def descriptive(type, array):
+    while len(type) < 6:
+        type = type + " "
+    print(f"{type} -- n={array.size}, min={array.min():.2f}, max={array.max():.2f}, mean={array.mean():.2f}, std dev={array.std():.2f}, peak to peak={array.ptp():.2f}")
+                
+def main():
+    if len(sys.argv) < 2:
+        root = tk.Tk()
+        root.withdraw()
+        filePath = filedialog.askopenfilename(
+            filetypes = [("Comma separated file (CSV) format",".csv")])
+    else:
+        parser = argparse.ArgumentParser(description=
+                                         "Descriptive statistics for accerometer file")
+        parser.add_argument("filename", help="Input filename")
+        args = parser.parse_args()
+        filePath = args.filename
+
+    processor = Processor()
+    processor.process(filePath)
+    descriptive("x", processor.x);
+    descriptive("y", processor.y);
+    descriptive("z", processor.z);
+    descriptive("total", processor.tot);
+    
+
+if __name__ == "__main__":
+    main()
+  
