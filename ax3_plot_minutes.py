88bf27a044ce528bba1369b44d025c7c3f8bf615
Jason Leake <jason@leake.me.uk>
2020-02-25 13:40:53 +0000

    Move plot specific vertical lines etc to an ini file to make plot more generally useful

--- ax3_plot_minutes.py
+++ ax3_plot_minutes.py
@@ -29,65 +29,117 @@
 #
 # Plot stats from ax3
 #
-
-import numpy as np
-import argparse
-import sys
 from tkinter import filedialog
-import tkinter as tk
+import argparse
+import configparser
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import tkinter as tk
+import os.path as path
 
 class Processor:
-    def process(self, filename):
-        with open(filename, "r") as csvfile:
-            reader = csv.reader(csvfile, delimiter=" ", quotechar="#")
-            for row in reader:
-                print(", ".join(row))
+
+    def process(self, filename, controlFile):
+        """Process a CSV file of per-minute data as produced by ax3_stats.py.
+        controlFile, if specified, is an INI file that controls the
+        generation of short plots as well as the normal ones. It contains
+        the following in the section [PLOT] -- start is the minute to
+        start "short" plot, end -- is the minute to end the "short" plot """
+    
+        config = configparser.ConfigParser()
+        if controlFile is not None:
+            if path.exists(controlFile):
+                config.read(controlFile)
+            else:
+                print(f"Control file {controlFile} not found", file=sys.stderr)
+                sys.exit(1)
+
+        # Numpy CSV reader
         data = np.genfromtxt(filename, delimiter=",")
         print(data)
 
         title = [ "minute", "size",
-                  "mean of x values over minute",
-                  "ptp of x values over minute",
-                  "rms of x values over minute",
-                  "std dev of x values over minute",
-                  "mean of y values over minute",
-                  "ptp of y values over minute",
-                  "rms of y values over minute",
-                  "std dev of y values over minute",
-                  "mean of z values over minute",
-                  "ptp of z values over minute",
-                  "rms of z values over minute",
-                  "std dev of z values over minute",
-                  "mean of tot values over minute",
-                  "ptp of tot values over minute",
-                  "rms of tot values over minute",
-                  "std dev of tot values over minute"]
+                  "Mean of x acceleration over minute",
+                  "peak to peak of x acceleration over minute",
+                  "RMS of x acceleration over minute",
+                  "std dev of x acceleration over minute",
+                  "Mean of y acceleration over minute",
+                  "peak to peak of y acceleration over minute",
+                  "RMS of y acceleration over minute",
+                  "std dev of y acceleration over minute",
+                  "Mean of z acceleration over minute",
+                  "peak to peak of z acceleration over minute",
+                  "RMS of z acceleration over minute",
+                  "std dev of z acceleration over minute",
+                  "Mean of tot acceleration over minute",
+                  "peak to peak of tot acceleration over minute",
+                  "RMS of tot acceleration over minute",
+                  "std dev of tot acceleration over minute"]
 
+        fileTitle = [ "", "",
+                  "mean_x.pdf",
+                  "peak_to_peak_x.pdf",
+                  "rms_x.pdf",
+                  "std_dev_x.pdf",
+                  "mean_y.pdf",
+                  "peak_to_peak_y.pdf",
+                  "rms_y.pdf",
+                  "std_dev_y.pdf",
+                  "mean_z.pdf",
+                  "peak_to_peak_z.pdf",
+                  "rms_z.pdf",
+                  "std_dev_z.pdf",
+                  "mean_total.pdf",
+                  "peak_to_peak_total.pdf",
+                  "rms_total.pdf",
+                  "std_dev_total.pdf"]
                   
-        # All the interesting stuff happens in the first 25 minutes
-        truncated = data[:40]
-        for index in range(2, len(title)):
-            plt.title(title[index])
-            plt.axvline(4,c="red")
-            plt.axvline(12,c="red")
-            plt.axvline(20,c="red")
-            plt.xlabel("minute")
-            plt.plot(truncated[:, [index]])
-            plt.show()
+        # All the interesting stuff happens in the first few mins in
+        # some datasets
+        if config.has_section("PLOT"):
+            if config.has_option("PLOT", "start"):
+                start = int(config.get("PLOT", "start"))
+            else:
+                start = 0
+
+            if config.has_option("PLOT", "end"):
+                end = int(config.get("PLOT", "end"))
+            else:
+                start = len(data) - 1
+
+            if config.has_option("PLOT", "xlines"):
+                xlines = config.get("PLOT","xlines")
+            else:
+                xlines = None
+            xlinesList = xlines.split(',')            
+            truncated = data[start:end]
+            for index in range(2, len(title)):
+                plt.title(title[index])
+                for line in xlinesList:
+                    line = int(line.strip())
+                    plt.axvline(line ,c="red")
+                plt.xlabel("minute")
+                plt.plot(truncated[:, [index]])
+                plt.savefig(f"short_{fileTitle[index]}.pdf")
+                plt.show()
+        else:
+            xlinesList = []
 
         # For the full dataset
         for index in range(2, len(title)):
             plt.title(title[index])
-            plt.axvline(4,c="red")
-            plt.axvline(12,c="red")
-            plt.axvline(20,c="red")
+            for line in xlinesList:
+                line = int(line.strip())
+                plt.axvline(line ,c="red")
             plt.xlabel("minute")
             plt.plot(data[:, [index]])
+            plt.savefig(f"long_{fileTitle[index]}.pdf")
             plt.show()
             
 def main():
+    controlFile = None
     if len(sys.argv) < 2:
         root = tk.Tk()
         root.withdraw()
@@ -97,11 +149,13 @@ def main():
         parser = argparse.ArgumentParser(description=
                                          "Plot statistics for accelerometer file")
         parser.add_argument("filename", help="Input filename")
+        parser.add_argument("--controlfile", nargs="?", help="INI file to control plotting")
         args = parser.parse_args()
         filePath = args.filename
-
+        controlFile = args.controlfile
+        
     processor = Processor()
-    processor.process(filePath)
+    processor.process(filePath, controlFile)
     
 if __name__ == "__main__":
     main()
