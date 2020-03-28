b39e8022d8e9c7b795a392846a979c57a5b18053
Jason Leake <Jason Leake>
2020-03-28 21:01:52 +0000

    Add

---
+++ ax3_wearing.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+# coding=UTF-8
+#
+# BSD 2-Clause License
+#
+# Copyright (c) 2019, Jason Leake
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Alex's filter for detecting sleeve not worn.
+#
+# "My thought is to apply a filter which takes the highest
+# acceleration value for each second and presents this. We could then
+# apply a threshold value from sample data that would classify
+# movement or not. Periods of say longer than 10 mins with no value
+# over threshold could then be classed as 'not wearing' (or completely
+# immobile)."
+#
+
+# Axes:
+# X is the long axis
+# Y is the across-the-device axis
+# Z is across the thickness of the device
+import numpy as np
+import argparse
+import sys
+from tkinter import filedialog
+import tkinter as tk
+import csv
+from Row import Row
+import os
+
+class Processor:
+
+    def makeOutFile(self, filename, threshold):
+        """ Make output filename """
+        path, file = os.path.split(filename)
+        name, ext = os.path.splitext(file)
+        newName = "wearing_" + str(threshold) + "_" + name;
+        
+        newPlotName = newName + ".pdf"
+        newName = newName + ".csv"
+        
+        plotFilePath = os.path.join(path, newName)
+        dataFilePath = os.path.join(path, newPlotName)
+        print("Plot file is", plotFilePath)
+        print("Output file is", dataFilePath)
+        return [plotFilePath, dataFilePath]
+
+    def _plot(self, plotFilename,
+              maxAccPerSecond,
+              aboveThresholdValues,
+              movementDetected):
+        pass
+              
+    def __call__(self, filename, threshold):
+        """ Process the file """
+        # Count number of seconds in the file
+        count = 0
+        firstEpoch = None
+        lastEpoch = None
+        with open(filename, "rt", newline="\n") as self.fh:
+            line = self.fh.readline().strip()
+            while line:
+                row = Row(line)
+                if row.skip:
+                    pass
+                elif firstEpoch is None:
+                    firstEpoch = row.getEpoch()
+                line = self.fh.readline().strip()
+                
+            lastEpoch = row.getEpoch()
+        secondsInFile = int(lastEpoch) - int(firstEpoch) + 1
+        print(f"File contains {secondsInFile} seconds worth of data")
+        
+        maxAccPerSecond = np.zeros(secondsInFile)
+        firstSecondEpoch = int(firstEpoch)
+
+        # Now scan through the file and pick out the highest acceleration
+        # in each second
+        with open(filename, "rt", newline="\n") as self.fh:
+            line = self.fh.readline().strip()
+            while line:
+                row = Row(line)
+                if row.skip:
+                    pass
+                else:
+                    second = int(row.getEpoch()) - firstSecondEpoch
+                    totalAcc = row.getTotAcc()
+                    if totalAcc > maxAccPerSecond[second]:
+                        maxAccPerSecond[second] = totalAcc
+                line = self.fh.readline().strip()
+        print(f"Max per second accelerations extracted")
+        aboveThresholdValues = maxAccPerSecond
+        # Now scan through looking for movement above a threshold
+        for second in range(len(aboveThresholdValues)):
+            if aboveThresholdValues[second] < threshold:
+                aboveThresholdValues[second] = 0
+
+        print(f"Determining periods of movement")
+        # Scan through the above threshold values to set +/-5 minutes to
+        # 1 when there is an above threshold value
+        movementDetected = np.zeros(secondsInFile)
+        for second in range(len(aboveThresholdValues)):
+            if aboveThresholdValues[second] > 0:
+                # Set the previous 5 * 60 values, less one, to 1
+                backIndex = second - ((4 * 60) + 59)
+                for index in range(backIndex, second):
+                    movementDetected[index] = 1
+                # Set the next 5 * 60 values to 1
+                for index in range(second, second + (5*60)):
+                    aboveThresholdValues[second] = 1
+
+        plotFilename, outputFilename = self.makeOutFile(filename, threshold)
+        self._plot(plotFilename,
+                   maxAccPerSecond,
+                   aboveThresholdValues,
+                   movementDetected)
+        
+        # Output this as a data file
+        outfile = open(outputFilename, "w")
+        with outfile:
+            writer = csv.writer(outfile)
+            for second in range(len(aboveThresholdValues)):
+                writer.writerow([int(firstEpoch) + second,
+                                 second,
+                                 maxAccPerSecond[second],
+                                 aboveThresholdValues[second],
+                                 movementDetected[second]])
+        return [plotFilename, outputFilename]
+    
+def main():
+    """ Command line entry point
+    """
+    parser = argparse.ArgumentParser(description=
+                                     "Descriptive statistics for accelerometer file")
+    parser.add_argument("filename", help="Input filename")
+    parser.add_argument("threshold", help="Threshold",
+                        type=float, default="0.1")
+    args = parser.parse_args()
+    filePath = args.filename
+    name, extension =  os.path.splitext(filePath)
+
+    if extension == ".CWA":
+        print("You need the .csv, not the .CWA", file=stderr)
+        os.exit(0)
+            
+    processor = Processor()
+    plotfile, datafile = processor(args.filename, args.threshold)
+    
+if __name__ == "__main__":
+    main()
+
